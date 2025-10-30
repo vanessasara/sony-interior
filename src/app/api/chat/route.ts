5b121d96f3d0c6f404107bc203d766aac0d49d8d
venisa sara <127692218+vanessasara@users.noreply.github.com>
2025-10-30 23:44:50 +0500

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,18 +1,34 @@
-import { generateText } from "ai";
+import { streamText, UIMessage, convertToModelMessages } from "ai";
 import { google } from "@ai-sdk/google";
 
 export async function POST(req: Request) {
   try {
-    const { messages } = await req.json();
+    const { messages }: { messages: UIMessage[] } = await req.json();
 
-    const { text } = await generateText({
+    const result = await streamText({
       model: google("gemini-2.5-flash"),
-      messages,
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a helpful furniture assistant that helps users to find the best furnitures",
+        },
+        ...convertToModelMessages(messages),
+      ],
     });
 
-    return Response.json({ text });
+    result.usage.then((usage) => {
+      console.log({
+        messageCount: messages.length,
+        inputTokens: usage.inputTokens,
+        outputTokens: usage.outputTokens,
+        totalTokens: usage.totalTokens,
+      });
+    });
+
+    return result.toUIMessageStreamResponse();
   } catch (error) {
-    console.error("Error generating text:", error);
-    return Response.json({ error: "Failed to generate text" }, { status: 500 });
+    console.error("Error streaming chat completion:", error);
+    return new Response("Failed to stream chat completion", { status: 500 });
   }
 }
